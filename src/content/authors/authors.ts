04b12d26fc76fa2901bde35724739471ad0646b0
keytelpumaylle <201286@unamba.edu.pe>
2026-01-21 17:10:21 -0500

    Restore Photos file from Authors

--- src/content/authors/authors.ts
+++ src/content/authors/authors.ts
@@ -4,8 +4,7 @@ export const authors = {
   "yonatan-mamani": {
     name: "Yonatan Mamani Coaquira ",
     role: "Profesor en Inteligencia Artifical",
-    avatar:
-      "https://iaunamba.github.io/author/yonatan-mamani-coaquira/avatar_hu12466553930844653061.png",
+    avatar: "https://avatars.githubusercontent.com/u/6805866?v=4",
     bio: "Experto en Machine learning",
     social: {
       twitter: "https://twitter.com/juan",
@@ -16,7 +15,7 @@ export const authors = {
     name: "Keytel Pumaylle Ramirez",
     role: "Estudiante de Ingenieria Informatica y Sitemas",
     avatar:
-      "https://iaunamba.github.io/author/keytel-pumaylle-ramirez/avatar_hu7137933794213440928.jpg",
+      "https://scontent-lim1-1.xx.fbcdn.net/v/t39.30808-6/371780237_1008787330259246_2739041328903563815_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeH4LqdWhPWJZzCOrSPvpWhV6ph1Dmyx5mDqmHUObLHmYO3GlQ6Pj3akgaFXHiSDNvNczPpTxgdYjToX1DDvAY1y&_nc_ohc=F1JFS2dJelEQ7kNvwF2dY3-&_nc_oc=AdlGZ_-xy02qUlZMcwoq5FCtpJX5A1C1dtaDA7D9hguRS5TAtpCHPJzkUmjTK2fa8dI&_nc_zt=23&_nc_ht=scontent-lim1-1.xx&_nc_gid=1B8OPjhXyunNYJtuAt4xBQ&oh=00_AfrsSfkpGbq12nOmZj_gztnmT3BRaNfOVEKrAIgOyfjCkg&oe=69770A3B",
     bio: "Estudiante",
     social: {
       twitter: "https://twitter.com/maria",
@@ -27,6 +26,26 @@ export const authors = {
     role: "Estudiante",
     avatar:
       "https://scontent.flim2-6.fna.fbcdn.net/v/t1.6435-9/205402316_108048701527969_8442380963064787801_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeEmrGcf5zmq7bT0v9Tuma2tIEFIMaS-bRogQUgxpL5tGhYiFGdwJukl9JvhHBZsvl9vHPs1sduztvJw09k8mGCr&_nc_ohc=NYpp-uGhLj8Q7kNvwE4-Fyg&_nc_oc=Adll_pFzEwnk04fqJjRpavxzY8SFfpmnGcskm-gvp54fDpjhsXOXcvHwiZ0cEhDbaVI&_nc_zt=23&_nc_ht=scontent.flim2-6.fna&_nc_gid=hhbWXmSzFTZo7U9JYLri5Q&oh=00_Afk-lwjwgTJLCprpPj2FuPMmk0AtpFv0MU4dS1qECffmIQ&oe=697637B3",
+    bio: "Estudiante",
+    social: {
+      twitter: "",
+    },
+  },
+  "rafael-merma": {
+    name: "Rafael R. Quispe Merma",
+    role: "Profesor de Big Data",
+    avatar:
+      "https://i.pinimg.com/736x/ea/67/8c/ea678c8b25b9dcbe080dda13594533d0.jpg",
+    bio: "Estudiante",
+    social: {
+      twitter: "",
+    },
+  },
+  "jeymi-gomez": {
+    name: "Jeymi J. Gomez Coaquira",
+    role: "Estudiante",
+    avatar:
+      "https://i.pinimg.com/736x/ea/67/8c/ea678c8b25b9dcbe080dda13594533d0.jpg",
     bio: "Estudiante",
     social: {
       twitter: "",
